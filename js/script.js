bf3efba7a1b9da671a8854222a9d6da225ca77d5
Victor <iwenofuvictor2016@gmail.com>
2026-07-29 13:00:28 -0700

    Built about section and testimonials

---
+++ js/script.js
@@ -0,0 +1,110 @@
+// Testimonial Carousel Javascript
+
+const track = document.querySelector(".testimonial-track");
+
+const cards = document.querySelectorAll(".testimonial-card");
+
+const nextBtn = document.querySelector(".next");
+
+const prevBtn = document.querySelector(".prev");
+
+
+function getCardsPerView(){
+
+    if(window.innerWidth <= 768){
+
+        return 1;
+
+    }
+
+    if(window.innerWidth <= 992){
+
+        return 2;
+
+    }
+
+    return 3;
+
+}
+
+let currentIndex = 0;
+
+let cardsPerView = getCardsPerView();
+
+function updateSlider(){
+
+    cardsPerView = getCardsPerView();
+
+    const cardWidth = cards[0].offsetWidth;
+
+    track.style.transform =
+        `translateX(-${currentIndex * cardWidth}px)`;
+
+}
+
+nextBtn.addEventListener("click",()=>{
+
+    if(currentIndex < cards.length - cardsPerView){
+
+        currentIndex++;
+
+    }else{
+
+        currentIndex = 0;
+
+    }
+
+    updateSlider();
+
+});
+
+prevBtn.addEventListener("click",()=>{
+
+    if(currentIndex > 0){
+
+        currentIndex--;
+
+    }else{
+
+        currentIndex = cards.length - cardsPerView;
+
+    }
+
+    updateSlider();
+
+});
+
+window.addEventListener("resize",()=>{
+
+    cardsPerView = getCardsPerView();
+
+    updateSlider();
+
+});
+
+setInterval(()=>{
+
+    nextBtn.click();
+
+},10000);
+
+// stop card slide on hover
+
+let autoSlide = setInterval(() => {
+    nextBtn.click();
+}, 10000);
+
+const slider = document.querySelector(".testimonial-slider");
+
+slider.addEventListener("mouseenter", () => {
+    clearInterval(autoSlide);
+});
+
+slider.addEventListener("mouseleave", () => {
+    autoSlide = setInterval(() => {
+        nextBtn.click();
+    }, 10000);
+});
+
+
+
