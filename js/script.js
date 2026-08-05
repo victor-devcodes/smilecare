a85ae237e1fe560b225d8552664a80c1767ea722
Victor <iwenofuvictor2016@gmail.com>
2026-08-05 06:37:35 -0700

    completed services page, full website completed, remaining responsiveness

--- js/script.js
+++ js/script.js
@@ -269,3 +269,22 @@ function initLightbox() {
 
     });
 }
+
+/*==================================================
+    FAQ
+==================================================*/
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item=>{
+
+    const question = item.querySelector(".faq-question");
+
+    question.addEventListener("click",()=>{
+
+        item.classList.toggle("active");
+
+    });
+
+});
+
